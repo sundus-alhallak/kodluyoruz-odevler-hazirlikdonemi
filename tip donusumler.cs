c7ad5c6ccb71294d8fcda411aa6b3ddf81340542
Sundus MERCAN <77814446+sundus-alhallak@users.noreply.github.com>
2022-02-27 01:51:23 +0300

    Add files via upload

---
+++ tip donusumler.cs
@@ -0,0 +1,35 @@
+using system;
+namespace tip_donusumlari{
+class program
+
+{
+
+    static void Main (string[]args){
+
+        byte a=4;
+        sbyte b =30;
+        short c=19;
+        int d=a+b+c;
+        console.writeline("d:"+d);
+
+        long h =d;
+         console.writeline("h:"+h);
+         folat i =h ;
+          console.writeline("i:"+i);
+          string e ="sundus";
+          char f='k';
+          obect g = e+f+d;
+           console.writeline("g:"+g);
+           console.writeline();
+                int x =4 ;
+                byte y = (byte)x;
+                Console.writeline("y"+y)
+                     int y =4 ;
+                byte z = (byte)y; 
+                 Console.writeline("z"+z)  
+                  int w =4 ;
+                byte v = (byte)w;
+ Console.writeline("v"+v)
+
+    }
+}}
