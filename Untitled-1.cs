77fa9404310a193d3d2103e4d949df38ec7dcaa5
Sundus MERCAN <77814446+sundus-alhallak@users.noreply.github.com>
2022-02-27 00:34:12 +0300

    Add files via upload

---
+++ Untitled-1.cs
@@ -0,0 +1,51 @@
+
+using system ;
+namespace degisken {
+
+    class program {
+
+        static void Main (string [] args ){
+         console.Writeline ("");
+         byte b =5;
+         sbyte   c =5;
+         short s = 5; 
+         ushort d = 5 ;
+         Int16 = 2;
+         Int32 = 5 ;
+         Int64 =7 ;
+         //reel sayilar 
+         folat f =8;
+         double g = 66;// 8 bayte
+         decimal de =3; 
+         char ch ='5'; //2 byte
+         string str ="zaas"// sinirsiz
+         bool b1= true;
+         boolb2 = false ;
+         DateTime dt= DateTime.now ;
+         console.Writeline(dt);
+          
+          //String ifadeler 
+          int integer1 =5;
+          int integer2=6;
+          int integer3 = integer1 +integer2;
+          // boolean
+          bool bool1 =10>2:
+          // degisken donusumler 
+          String dtr20 ="20";
+          int int20 =20;
+          String yenideger=str20+ int20.ToString();
+          console.writeline (yenideger);
+          int int21 =int20 + cover.ToString();
+          console.wruteline(int21);
+          int22 = int20 +int.parse(str20);
+          //Datetime 
+          String datetime =DateTime.now.ToString("dd.mm.yyyy")
+console.writeline("datetime");
+
+
+        
+
+        }
+    }
+
+}
---
+++ hafta1 hazirlik.cs
@@ -0,0 +1,19 @@
+usind system;
+namespace console_programlama 
+{
+ class program 
+ {
+ public ststic void Main (string[]args ){
+
+     Console.writeline("hello");
+      Console.writeline("isminizi giriniz ");
+      string name =Console.ReadLine();
+      Console.writeline("soyadinizi giriniz");
+      String surname = Console.ReadLine();
+      console.writeline(+name +""+surname);
+
+ }
+
+ }
+
+}
